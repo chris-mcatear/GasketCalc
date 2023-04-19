2ec1458ccd041287c8e07e3096aa47c607714bdc
Chris McAtear <christopher.mcatear@howden.com>
2023-04-19 16:20:57 +0100

    Updates to file validation

--- pandclass.py
+++ pandclass.py
@@ -16,9 +16,21 @@ class ExcelToPandas():
             #print("File path not correctly defined")
             messagebox.askretrycancel(title="File Path Definition Error", message="File path not correctly defined.")
         else: 
+            excel_valid = [False, False, False, False]
+            file_validator = ["Part Number", "Unit QTY", "QTY", "Description"]
             excel_df = pd.read_excel(self.filepath)
-            excel_df_column_titles = excel_df.columns
-            print(excel_df_column_titles)
+            print(excel_df.head())
+            for i in range(len(excel_df.columns)):
+                if excel_df.columns[i] != file_validator[i]:
+                    excel_valid[i] = False
+                    print("File Not Valid")
+                else: 
+                    print("File confirmed Valid")
+                    excel_valid[i] = True
+            print(excel_valid)
+            # excel_df_column_titles = excel_df.columns
+            # print(f"colums index: {excel_df.index}")
+            # print(f"column titles: {excel_df_column_titles}")
             #expected_column_title = ["Part Number", "Unit QTY", "QTY", "Description"]
             # for column in excel_df_column_titles:
             #     if excel_df_column_titles[column] != expected_column_title[column]:
