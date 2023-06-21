2f1a5e2fdc62ab45d15a1754ffa1759a08c65a17
Chris McAtear <christopher.mcatear@howden.com>
2023-06-21 15:27:54 +0100

    updates to merging, now includes CW, OIL and both Gas

--- bomparser.py
+++ bomparser.py
@@ -22,6 +22,7 @@ from tkinter import *
 from tkinter import filedialog as fd
 from tkinter.messagebox import showinfo
 from tkinter import messagebox
+import pandas as pd
 from pandclass import ExcelToPandas
     
 #Define window
@@ -29,7 +30,8 @@ window = tk.Tk()
 window.title('BoM Parser - ALPHA 0.0.1')
 # window.resizable(False, False)
 # window.geometry("1000x150")
-window.minsize(width=750, height=150)
+window.config(padx=25, pady=25)
+window.minsize(height=500)
 
 #Defining Style of Window
 #style = darkstyle(window)
@@ -50,6 +52,7 @@ def browsefunc():
         etop.filepath = filename
         # print(filename)
         filepathtext.config(text=filename)
+        
         #showinfo(title="ExcelToPandas File Path", message=etop.filepath)
         if len(filename) > 0:
             if etop.pandasfileapprove() == True:
@@ -72,47 +75,66 @@ def on_closing():
 def calculatefunc():
     """Usage: calculatefunc(input) / Will take input of file path and pass to pandas to interperetation, pandas to return details to display() function for displaying information."""
     # etop.gasket_series()
-    # etop.oil_gaskets()
+    oil_1_gaskets, oil_2_gaskets = etop.oil_gaskets()
     gas_1_gaskets, gas_2_gaskets = etop.gas_gaskets()
+    cw_gaskets = etop.water_gaskets()
+    
+    merged_gaskets_master = [gas_1_gaskets, gas_2_gaskets, oil_1_gaskets, oil_2_gaskets, cw_gaskets]
+    final_grouping = pd.concat(merged_gaskets_master)
+    # print(gas_1_gaskets)
     # print(gas_2_gaskets)
-    # etop.df_to_excel(gas_1_gaskets)
+    # print(cw_gaskets)
+    print(final_grouping)
+    return final_grouping
    
     
 def export_to_excel():
-    # etop.
-    pass
+    merged_export = calculatefunc()
+    etop.df_to_excel(merged_export)
     
     
 def popup_window():
     results_window = Toplevel(window)
-    results_window.minsize(height=500, width=750)
+    results_window.minsize(height=500, width=1000)
     results_window.title("Results")
     
+    gas_1_export, gas_2_export, merged_export, cw_gaskets = calculatefunc()
+    
+    tree = ttk.Treeview(results_window)
+    tree["columns"] = tuple(merged_export.columns)
+    # tree.heading=('#0', "Part Number")
+    
+    for column in merged_export.columns:
+        tree.column(column, width=100, anchor="w")
+        tree.heading(column, text=column)
+        
+    for index, row in merged_export.iterrows():
+        tree.insert("", "end", text=index, values=tuple(row))
+        
+    tree.pack(expand=True, fill="both")
+
 
 # Widgets    
-filepathframe = ttk.LabelFrame(window, text="Filepath: ")
+filepathframe = ttk.LabelFrame(text="Filepath: ")
 greeting = ttk.Label(text="Please choose a file")
-browserbutton = ttk.Button(text="Browse", command=browsefunc)
-calculatebutton = ttk.Button(text="Calculate", command=calculatefunc)
-# calculatebutton = ttk.Button(text="Calculate", command=popup_window)
-skip_choicebutton = ttk.Button(text="Skip Choice", command=calculatefunc)
-filepathtext = ttk.Label(filepathframe, text="Awaiting file selection.", width=100)
+browserbutton = ttk.Button(text="Browse", command=browsefunc, width=15)
+calculatebutton = ttk.Button(text="Calculate", command=calculatefunc, width=15)
+skip_choicebutton = ttk.Button(text="Skip Choice", command=calculatefunc, width=15)
+filepathtext = ttk.Label(filepathframe, text="Awaiting file selection.", width=70)
 file_approved = ttk.Label(text="Awaiting file selection.")
-# retrybutton = ttk.Button(text="Retry", command=browsefunc)
-# filepath = tk.Text(filepathtext, width=100, height=100)
-# filepath.grid(row=0, column=0)
-export_button = ttk.Button(text="Export to Excel", command=export_to_excel)
+export_button = ttk.Button(text="Export to Excel", command=export_to_excel, width=15)
+preview_button = ttk.Button(text="Preview data", command=popup_window, width=15)
+
 
 # Content Layout in window
-greeting.grid(row=0, column=0, padx=5, pady=5)
-filepathframe.grid(row=1, column=1, padx=10, pady=5)
-filepathtext.grid(row=1, column=2, padx=5, pady=5)
-browserbutton.grid(row=1, column=0, padx=10, pady=5)
-calculatebutton.grid(row=2, column=0, padx=10, pady=5)
-file_approved.grid(row=2, column=1)
-skip_choicebutton.grid(column=0, row=3)
+filepathframe.grid(row=1, column=0, columnspan=6, padx=25, pady=25)
+filepathtext.grid(row=1, column=0, columnspan=3, padx=10, pady=5)
+browserbutton.grid(row=0, column=0)
+calculatebutton.grid(row=0, column=1)
+file_approved.grid(row=2, column=0, columnspan=6)
+skip_choicebutton.grid(column=2, row=0)
+export_button.grid(column=4, row=0)
+preview_button.grid(column=5, row=0)
 
-#Window Remains on Screen
-#window.protocol("WM_DELETE_WINDOW", on_closing)
 
 window.mainloop()
--- pandclass.py
+++ pandclass.py
@@ -5,6 +5,12 @@ from tkinter import filedialog as fd
 from tkinter.messagebox import showinfo
 from tkinter import messagebox
 import pandas as pd
+from openpyxl import *
+from xlsxwriter import *
+
+END_RESULT_DF = pd.DataFrame()
+
+
 
 class ExcelToPandas():
     def __init__(self):
@@ -38,76 +44,98 @@ class ExcelToPandas():
                 return False
 
             
-    def gasket_series(self):
-        # print("Starting Gasket Count")
-        # print(self.filepath)
-        excel_df = pd.read_excel(self.filepath)
-        # print(type(excel_df))
-        # print(excel_df.head())
-        gasket_values = excel_df[excel_df["Part Number"].str.contains("GASKET", na=False)]
-        # print(gasket_values)
-        three_hundred_pound = gasket_values[gasket_values["Part Number"].str.contains("#300", na=False)]
-        # print(three_hundred_pound)
-        # FROM HERE OUT THREE HUNDRED POUND FLANGE VAR WLL BE SHORTENED TO THP_*NAME* E.G. THP_ONE_INCH
-        thp_half_inch = three_hundred_pound[three_hundred_pound["Description"].str.contains("1/2'", na=False)]
-        thp_three_quart_inch = three_hundred_pound[three_hundred_pound["Description"].str.contains("3/4'", na=False)]
-        thp_one_inch = three_hundred_pound[three_hundred_pound["Description"].str.contains("1'", na=False)]
-        thp_one_half_inch = three_hundred_pound[three_hundred_pound["Description"].str.contains("1 1/2'", na=False)]
-        thp_two_inch = three_hundred_pound[three_hundred_pound["Description"].str.contains("2'", na=False)]
-        thp_three_inch = three_hundred_pound[three_hundred_pound["Description"].str.contains("3'", na=False)]
-        thp_six_inch = three_hundred_pound[three_hundred_pound["Description"].str.contains("6'", na=False)]
-        thp_eight_inch = three_hundred_pound[three_hundred_pound["Description"].str.contains("8'", na=False)]
-        thp_ten_inch = three_hundred_pound[three_hundred_pound["Description"].str.contains("10'", na=False)]
-        thp_twelve_inch = three_hundred_pound[three_hundred_pound["Description"].str.contains("12'", na=False)]
+    # def gasket_series(self):
+    #     # print("Starting Gasket Count")
+    #     # print(self.filepath)
+    #     excel_df = pd.read_excel(self.filepath)
+    #     # print(type(excel_df))
+    #     # print(excel_df.head())
+    #     gasket_values = excel_df[excel_df["Part Number"].str.contains("GASKET", na=False)]
+    #     # print(gasket_values)
+    #     three_hundred_pound = gasket_values[gasket_values["Part Number"].str.contains("#300", na=False)]
+    #     # print(three_hundred_pound)
+    #     # FROM HERE OUT THREE HUNDRED POUND FLANGE VAR WLL BE SHORTENED TO THP_*NAME* E.G. THP_ONE_INCH
+    #     thp_half_inch = three_hundred_pound[three_hundred_pound["Description"].str.contains("1/2'", na=False)]
+    #     thp_three_quart_inch = three_hundred_pound[three_hundred_pound["Description"].str.contains("3/4'", na=False)]
+    #     thp_one_inch = three_hundred_pound[three_hundred_pound["Description"].str.contains("1'", na=False)]
+    #     thp_one_half_inch = three_hundred_pound[three_hundred_pound["Description"].str.contains("1 1/2'", na=False)]
+    #     thp_two_inch = three_hundred_pound[three_hundred_pound["Description"].str.contains("2'", na=False)]
+    #     thp_three_inch = three_hundred_pound[three_hundred_pound["Description"].str.contains("3'", na=False)]
+    #     thp_six_inch = three_hundred_pound[three_hundred_pound["Description"].str.contains("6'", na=False)]
+    #     thp_eight_inch = three_hundred_pound[three_hundred_pound["Description"].str.contains("8'", na=False)]
+    #     thp_ten_inch = three_hundred_pound[three_hundred_pound["Description"].str.contains("10'", na=False)]
+    #     thp_twelve_inch = three_hundred_pound[three_hundred_pound["Description"].str.contains("12'", na=False)]
         
-        one_hundred_fifty_pound = gasket_values[gasket_values["Part Number"].str.contains("#150", na=False)]
-        # FROM HERE OUT ONE HUNDRED FIFTY POUND FLANGE VAR WLL BE SHORTENED TO OHFP_*NAME* E.G. OHFP_ONE_INCH
-        ohfp_half_inch = one_hundred_fifty_pound[one_hundred_fifty_pound["Description"].str.contains("1/2'", na=False)]
-        ohfp_three_quart_inch = one_hundred_fifty_pound[one_hundred_fifty_pound["Description"].str.contains("3/4'", na=False)]
-        ohfp_one_inch = one_hundred_fifty_pound[one_hundred_fifty_pound["Description"].str.contains("1'", na=False)]
-        ohfp_one_half_inch = one_hundred_fifty_pound[one_hundred_fifty_pound["Description"].str.contains("1 1/2'", na=False)]
-        ohfp_two_inch = one_hundred_fifty_pound[one_hundred_fifty_pound["Description"].str.contains("2'", na=False)]
-        ohfp_three_inch = one_hundred_fifty_pound[one_hundred_fifty_pound["Description"].str.contains("3'", na=False)]
-        ohfp_four_inch = one_hundred_fifty_pound[one_hundred_fifty_pound["Description"].str.contains("4'", na=False)]
-        ohfp_six_inch = one_hundred_fifty_pound[one_hundred_fifty_pound["Description"].str.contains("6'", na=False)]
-        ohfp_eight_inch = one_hundred_fifty_pound[one_hundred_fifty_pound["Description"].str.contains("8'", na=False)]
-        ohfp_ten_inch = one_hundred_fifty_pound[one_hundred_fifty_pound["Description"].str.contains("10'", na=False)]
-        ohfp_twelve_inch = one_hundred_fifty_pound[one_hundred_fifty_pound["Description"].str.contains("12'", na=False)]
+    #     one_hundred_fifty_pound = gasket_values[gasket_values["Part Number"].str.contains("#150", na=False)]
+    #     # FROM HERE OUT ONE HUNDRED FIFTY POUND FLANGE VAR WLL BE SHORTENED TO OHFP_*NAME* E.G. OHFP_ONE_INCH
+    #     ohfp_half_inch = one_hundred_fifty_pound[one_hundred_fifty_pound["Description"].str.contains("1/2'", na=False)]
+    #     ohfp_three_quart_inch = one_hundred_fifty_pound[one_hundred_fifty_pound["Description"].str.contains("3/4'", na=False)]
+    #     ohfp_one_inch = one_hundred_fifty_pound[one_hundred_fifty_pound["Description"].str.contains("1'", na=False)]
+    #     ohfp_one_half_inch = one_hundred_fifty_pound[one_hundred_fifty_pound["Description"].str.contains("1 1/2'", na=False)]
+    #     ohfp_two_inch = one_hundred_fifty_pound[one_hundred_fifty_pound["Description"].str.contains("2'", na=False)]
+    #     ohfp_three_inch = one_hundred_fifty_pound[one_hundred_fifty_pound["Description"].str.contains("3'", na=False)]
+    #     ohfp_four_inch = one_hundred_fifty_pound[one_hundred_fifty_pound["Description"].str.contains("4'", na=False)]
+    #     ohfp_six_inch = one_hundred_fifty_pound[one_hundred_fifty_pound["Description"].str.contains("6'", na=False)]
+    #     ohfp_eight_inch = one_hundred_fifty_pound[one_hundred_fifty_pound["Description"].str.contains("8'", na=False)]
+    #     ohfp_ten_inch = one_hundred_fifty_pound[one_hundred_fifty_pound["Description"].str.contains("10'", na=False)]
+    #     ohfp_twelve_inch = one_hundred_fifty_pound[one_hundred_fifty_pound["Description"].str.contains("12'", na=False)]
         
-        print(three_hundred_pound)
-        print(one_hundred_fifty_pound)
+    #     print(three_hundred_pound)
+    #     print(one_hundred_fifty_pound)
     
     
     def oil_gaskets(self):
         excel_df = pd.read_excel(self.filepath)
-        oil_gaskets_master = excel_df[excel_df["Part Number"].str.contains("OIL", na=False)]
-        print(oil_gaskets_master)
+        oil_gaskets_master_1 = excel_df[excel_df["Part Number"].str.contains("OIL 1", na=False)]
+        oil_gaskets_master_2 = excel_df[excel_df["Part Number"].str.contains("OIL 2", na=False)]
+        
+        grouped_oil_gaskets_1 = oil_gaskets_master_1.groupby(oil_gaskets_master_1["Part Number"]).agg({'QTY': 'sum', 'Description': '&&&'.join})
+        grouped_oil_gaskets_1['Description'] = grouped_oil_gaskets_1['Description'].apply(lambda x: x.split('&&&')[0])
+        
+        grouped_oil_gaskets_2 = oil_gaskets_master_2.groupby(oil_gaskets_master_2["Part Number"]).agg({'QTY': 'sum', 'Description': '&&&'.join})
+        grouped_oil_gaskets_2['Description'] = grouped_oil_gaskets_2['Description'].apply(lambda x: x.split('&&&')[0])
+        
+        return grouped_oil_gaskets_1, grouped_oil_gaskets_2
         
         
     def gas_gaskets(self):
         excel_df = pd.read_excel(self.filepath)
         gas_gaskets_master_1 = excel_df[excel_df["Part Number"].str.contains("GAS 1", na=False)]
         gas_gaskets_master_2 = excel_df[excel_df["Part Number"].str.contains("GAS 2", na=False)]
-        print(gas_gaskets_master_1)
+        # print(gas_gaskets_master_1)
         # print(gas_gaskets_master_2)
 
         # MERGE MASTER GASKET LIST BY PART NUMBER 
-        merged_gas_gaskets_1 = gas_gaskets_master_1.groupby(gas_gaskets_master_1["Part Number"]).agg({'QTY': 'sum', 'Description': '&&&'.join})
-        merged_gas_gaskets_1['Description'] = merged_gas_gaskets_1['Description'].apply(lambda x: x.split('&&&')[0])
+        grouped_gas_gaskets_1 = gas_gaskets_master_1.groupby(gas_gaskets_master_1["Part Number"]).agg({'QTY': 'sum', 'Description': '&&&'.join})
+        grouped_gas_gaskets_1['Description'] = grouped_gas_gaskets_1['Description'].apply(lambda x: x.split('&&&')[0])
+        
+        grouped_gas_gaskets_2 = gas_gaskets_master_2.groupby(gas_gaskets_master_2["Part Number"]).agg({'QTY': 'sum', 'Description': '&&&'.join})
+        grouped_gas_gaskets_2['Description'] = grouped_gas_gaskets_2['Description'].apply(lambda x: x.split('&&&')[0])
+        
         # merged_gas_gaskets_1 = merged_gas_gaskets_1.sort_values(by="Part Number")
-        print(f"\n \n \n \n Merged Gaskets: \n")
-        print(merged_gas_gaskets_1)
-        return gas_gaskets_master_1, gas_gaskets_master_2
+        # print(f"\n \n \n \n Merged Gaskets: \n")
+        # print(grouped_gas_gaskets_1)
+        # print(grouped_gas_gaskets_2)
+        
+        # merged_gaskets_master = [grouped_gas_gaskets_1, grouped_gas_gaskets_2]
+        # final_grouping = pd.concat(merged_gaskets_master)
+        # print(final_grouping)
+        
+        return grouped_gas_gaskets_1, grouped_gas_gaskets_2
     
     
     # Cooling water is C.W. ?? 
     def water_gaskets(self):
-        pass
+        excel_df = pd.read_excel(self.filepath)
+        cw_gaskets = excel_df[excel_df["Part Number"].str.contains("- CW", na=False)]
+        grouped_cw_gaskets = cw_gaskets.groupby(cw_gaskets["Part Number"]).agg({'QTY': 'sum', 'Description': '&&&'.join})
+        return grouped_cw_gaskets
     
     
-    # def df_to_excel(self, gasket_selection):
-    #     gasket_selection.to_excel()
-    
-    # https://stackoverflow.com/questions/46826773/how-can-i-merge-rows-by-same-value-in-a-column-in-pandas-with-aggregation-func
-    # this is how to combine dataframes to make them shorter, should be able to add this after master lists and get aggregated lists. 
-        
+    def df_to_excel(self, merged_export):
+        filetypes = (("Excel File", "*.xlsx"),)
+        filename = fd.asksaveasfilename(initialdir = "/",title = "Select file",filetypes = (("Excel File", "*.xlsx"),("all files","*.*")))
+        print(type(merged_export))
+        merged_export.to_excel(f'{filename}.xlsx')
+
+# CONDNESATE, ISOLATING
