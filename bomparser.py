f4ccc60bdc6ef52cab287988ca1198e4577f54f5
chris-mcatear <chris.mcatear@hotmail.com>
2025-04-21 11:50:33 +0100

    missed version update

--- bomparser.py
+++ bomparser.py
@@ -17,7 +17,7 @@ etop = ExcelToPandas()
 s_splitter = SpareSplitter()
 UpdateChecker = CheckforUpdate()
 
-current_version = "v1.1a"
+current_version = "v1.2a"
 release_version = UpdateChecker.updatechecker()
 
 
